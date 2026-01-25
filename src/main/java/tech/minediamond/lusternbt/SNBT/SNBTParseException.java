2f8a70dff3f4af2a19b37cab155137aa2d0a2d69
mineDiamond <mine_yaoym@outlook.com>
2026-01-25 19:32:47 +0800

    修改名称 (#11)

--- src/main/java/tech/minediamond/lusternbt/SNBT/SNBTParseException.java
+++
@@ -1,7 +0,0 @@
-package tech.minediamond.lusternbt.SNBT;
-
-public class SNBTParseException extends RuntimeException {
-    public SNBTParseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
---
+++ src/main/java/tech/minediamond/micanbt/conversion/builtin/LongArrayTagConverter.java
@@ -0,0 +1,19 @@
+package tech.minediamond.micanbt.conversion.builtin;
+
+import tech.minediamond.micanbt.conversion.TagConverter;
+import tech.minediamond.micanbt.tag.builtin.LongArrayTag;
+
+/**
+ * A converter that converts between LongArrayTag and long[].
+ */
+public class LongArrayTagConverter implements TagConverter<LongArrayTag, long[]> {
+    @Override
+    public long[] convert(LongArrayTag tag) {
+        return tag.getValue();
+    }
+
+    @Override
+    public LongArrayTag convert(String name, long[] value) {
+        return new LongArrayTag(name, value);
+    }
+}
