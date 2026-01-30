55e222250f50dff465ae627061dc42b81fee0a2e
mine_ <mine_yaoym@outlook.com>
2026-01-30 18:21:21 +0800

    feat: 拆分更多方法

--- src/main/java/tech/minediamond/micanbt/NBT/NBT.java
+++ src/main/java/tech/minediamond/micanbt/NBT/NBT.java
@@ -4,7 +4,6 @@ import tech.minediamond.micanbt.tag.builtin.CompoundTag;
 import tech.minediamond.micanbt.tag.builtin.Tag;
 
 import java.io.DataInput;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -12,42 +11,50 @@ import java.nio.file.Path;
 public class NBT {
 
     public static CompoundTag read(Path path) throws IOException {
-        return NBTIO.read(path);
+        return NBTReader.read(path);
+    }
+
+    public static CompoundTag read(Path path, boolean littleEndian) throws IOException {
+        return NBTReader.read(path, littleEndian);
     }
 
     public static CompoundTag read(Path path, boolean compressed, boolean littleEndian) throws IOException {
-        return NBTIO.read(path, compressed, littleEndian);
+        return NBTReader.read(path, compressed, littleEndian);
     }
 
     public static Tag prase(InputStream in) throws IOException {
-        return NBTIO.readTag(in);
+        return NBTReader.read(in);
+    }
+
+    public static CompoundTag read(InputStream in, boolean littleEndian) throws IOException {
+        return NBTReader.read(in, littleEndian);
     }
 
     public static Tag prase(InputStream in, boolean littleEndian) throws IOException {
-        return NBTIO.readTag(in, littleEndian);
+        return NBTReader.readTag(in, littleEndian);
     }
 
     public static Tag prase(DataInput in) throws IOException {
-        return NBTIO.readTag(in);
+        return NBTReader.readTag(in);
     }
 
-    public static Tag readAnyTag(InputStream in) throws IOException {
-        return NBTIO.readAnyTag(in);
-    }
-
-    public static Tag readAnyTag(InputStream in, boolean littleEndian) throws IOException {
-        return NBTIO.readAnyTag(in, littleEndian);
-    }
-
-    public static Tag readAnyTag(DataInput in) throws IOException {
-        return NBTIO.readAnyTag(in);
-    }
+//    public static Tag readAnyTag(InputStream in) throws IOException {
+//        return NBTReader.readAnyTag(in);
+//    }
+//
+//    public static Tag readAnyTag(InputStream in, boolean littleEndian) throws IOException {
+//        return NBTReader.readAnyTag(in, littleEndian);
+//    }
+//
+//    public static Tag readAnyTag(DataInput in) throws IOException {
+//        return NBTReader.readAnyTag(in);
+//    }
 
     public static void write(CompoundTag tag, Path path) throws IOException {
-        NBTIO.writeFile(tag, path);
+        NBTWriter.write(tag, path);
     }
 
     public static void write(CompoundTag tag, Path path, boolean compressed, boolean littleEndian) throws IOException {
-        NBTIO.writeFile(tag, path, compressed, littleEndian);
+        NBTWriter.write(tag, path, compressed, littleEndian);
     }
 }
